3bff438e5b154ed446aba66c745c88c1baf22f51
hjcho <hjcho@seegene.com>
2021-06-18 15:39:46 +0900

    multiple useStates in a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './Counter';
+import Info from './Info';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Info />
     </div>
   );
 }
