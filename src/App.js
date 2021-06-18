3218a7886668c858cafb5d923076cfff521acd22
hjcho <hjcho@seegene.com>
2021-06-18 17:26:39 +0900

    reducer with object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Counter from './Counter';
+import Info from './Info';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Info />
     </div>
   );
 }
-
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+};
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
 
-  useEffect(() => {
-    console.log('렌더링이 완료되었습니다.');
-    console.log({ name, nickname });
-
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const onChange = (e) => {
+    dispatch(e.target);
   };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+
+  const { name, nickname } = state;
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
