8990ad0559f899784523c15812baedbb6ff81512
hjcho <hjcho@seegene.com>
2021-06-18 15:46:40 +0900

    useEffect when the component is mounted

--- src/Info.js
+++ src/Info.js
@@ -7,7 +7,7 @@ const Info = () => {
   useEffect(() => {
     console.log('렌더링이 완료되었습니다.');
     console.log({ name, nickname });
-  });
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
